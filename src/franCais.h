6b710292bc2fd9e185345f0c015a582c228d1dc7
Frityet <ambhogal01@gmail.com>
2021-12-14 19:26:17 -0800

    updated the example

--- src/franCais.h
+++ src/franCais.h
@@ -5,67 +5,74 @@
 #ifndef FREN_C_H
 #define FREN_C_H
 
-#define registre        register
-#define non_signé       unsigned
-#define signé           signed
-#define longue          long
-#define petit           short
+#define registre            register
+#define non_signé           unsigned
+#define signé               signed
+#define longue              long
+#define petit               short
 
-#define nul             ((void *)0)
-#define variable        __auto_type
+#define nul                 ((void *)0)
+#define variable            __auto_type
 
-#define automatique     auto
-#define constante       const
-#define statique        static
-#define extérieur       extern
-#define explosif        volatile //yeah idk
-#define en_ligne        inline
+#define automatique         auto
+#define constante           const
+#define statique            static
+#define extérieur           extern
+#define explosif            volatile //yeah idk
+#define en_ligne            inline
 
-#define immeuble        struct
-#define ensemble        union
-#define énumération     enum
-#define typedéfini      typedef
+#define immeuble            struct
+#define ensemble            union
+#define énumération         enum
+#define typedéfini          typedef
 
-#define interrupteur    switch
-#define cas             case
-#define casse           break
-#define retourne        return
-#define pour            for
-#define défaut          default
-#define va              goto
-#define taille_de       sizeof
-#define tantque         while
-#define fais            do
-#define si              if
-#define sinon           else
+#define interrupteur        switch
+#define cas                 case
+#define casse               break
+#define retourne            return
+#define pour                for
+#define défaut              default
+#define va                  goto
+#define taille_de           sizeof
+#define tantque             while
+#define fais                do
+#define si                  if
+#define sinon               else
+#define vrai                1
+#define faux                0
 
 //MISC
 
-#define commence        {
-#define fin             }
-#define baguette        *
-#define est             ==
-#define plus_grand      >
-#define plus_petit      <
-#define égal            =
-#define augmente        ++
-#define décrément       --
-#define plus            +
-#define moins           -
-#define multiplie       *
-#define divise          /
-#define modulo          %
-#define obtiens         .
-#define obtenais        ->
-#define fait            ;
-#define et              ,
-#define sélectionne(i)  [i]
+#define commence            {
+#define fin                 }
+#define baguette            *
+#define addresse_de(var)    &var
+#define déreference(var)    *var
+#define est                 ==
+#define plus_grand          >
+#define plus_petit          <
+#define est_plus_grand      >=
+#define est_plus_petit      <=
+#define égal                =
+#define augmente(var)       ++var
+#define décrément(var)      --var
+#define plus                +
+#define moins               -
+#define multiplie           *
+#define divise              /
+#define modulo              %
+#define obtiens             .
+#define obtenais            ->
+#define fait                ;
+#define et                  ,
+#define sélectionne(i)      [i]
 
-typedéfini char         caractère;
-typedéfini int          entier;
-typedéfini float        nombreà_virgule;
-typedéfini double       deux;
-typedéfini void         vide;
+typedéfini char             caractère;
+typedéfini int              entier;
+typedéfini float            nombreà_virgule;
+typedéfini double           deux;
+typedéfini void             vide;
+typedéfini _Bool            booléen;
 
 //macros dont work properly with typedefs
 typedéfini unsigned long taille;
@@ -75,17 +82,17 @@ typedéfini unsigned long taille;
 #define la_entier_principal_de_programme    fonction(entier main, entier argc, caractère baguette baguette argv)
 
 fonction(extérieur entier puts, constante caractère baguette) fait
-#define imprime(fmt) puts(fmt)
+#define écris(fmt) puts(fmt)
 
 fonction(extérieur entier printf, constante caractère baguette fmt, ...) fait
-#define imprimef(fmt, ...) printf(fmt, __VA_ARGS__)
+#define écrisf(fmt, ...) printf(fmt, __VA_ARGS__)
 
-fonction(extérieur caractère baguette gets, caractère baguette str) fait
-#define donnée(str) gets(str)
+fonction(entier scanf, caractère baguette str, ...) fait
+#define donnée(str, ...) scanf(str, __VA_ARGS__)
 
 fonction(extérieur longue strtol, constante caractère baguette str, constante caractère baguette baguette endptr, entier base) fait
-fonction(extérieur taille strlen, constante caractère baguette str) fait
-#define ficelle_à_entier(str) strtol(str, nul, strlen(str)) /*wah wah cry harder abt muh strlen*/
+//fonction(extérieur taille strlen, constante caractère baguette str) fait
+#define ficelle_à_entier(str) strtol(str, nul, 0)
 
 
 #endif //FREN_C_H
--- src/main.c
+++ src/main.c
@@ -1,11 +1,31 @@
 #include "franCais.h"
 
-#define nimi {
+fonction(booléen est_première, entier nombre) commence
+    si (nombre est_plus_petit 1)
+        retourne faux fait
+
+    pour (variable i égal 2 fait i plus_petit nombre fait augmente(i))
+        si (nombre modulo i est 0)
+            retourne faux fait
+
+    retourne vrai fait
+fin
 
 la_entier_principal_de_programme commence
-    caractère donnée[64] fait
-    donnée(donnée) fait
-
-    variable nombre égal ficelle_à_entier(donnée) fait
-
+    caractère tampon sélectionne(64) fait
+    écris("Entrez-vous un nombre!") fait
+    donnée("%s", tampon) fait
+    variable nombre égal ficelle_à_entier(tampon) fait
+    écrisf("Les première facteurs de %ld est\n", nombre) fait
+    pour (variable i égal 1 fait i est_plus_petit nombre fait augmente(i)) commence
+        si (nombre modulo i est 0) commence
+            si (est_première(i)) commence
+                si (i est nombre) commence
+                    écris("Cet nombre est première!") fait
+                    retourne 0 fait
+                fin
+                écrisf("%d\n", i) fait
+            fin
+        fin
+    fin
 fin
